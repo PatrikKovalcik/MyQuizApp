7936724656175f2a5a95e6a3f09307419019e752
Patrik Kovalcik <kovalcikpatrikk@gmail.com>
2023-05-18 08:46:05 +0200

    Questions

---
+++ app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programing language?",
+            "Where you are watching this video?"
+    };
+
+    public static String choices[][] ={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"}
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Youtube"
+    };
+}
